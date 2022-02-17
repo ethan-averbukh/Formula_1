0b876657dc3f9baf6d8d238f67f0a0961345205c
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-02-16 19:44:24 -0700

    Added Routing to App.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Home/HomePage";
+import About from "./About/About";
+import Contact from "./Contact/Contact";
+import Carousel from "./Carousel/Carousel";
 
 // MVP: User will be able to select year 2021 2022 and schedule, standings, and circuit tracks.
 
@@ -24,17 +27,29 @@ import Home from "./Home/HomePage";
 function App() {
   const [selectedYear, setSelectedYear] = useState("");
   const [dataType, setDataType] = useState("");
+  const [races, setRaces] = useState([]);
+  const [drivers, setDrivers] = useState([]);
 
-  // Use Effect runs API call; API call takes in two user inputs: 1)Data Type, 2) Year; useEffect is set to track state variable changes to: selectedYear and dataType
-  // Conditional checks to make sure state variables are not empty strings, and then runs the API Call.
+  /*  Use Effect runs API call; API call takes in two user inputs: 1)Data Type, 2) Year; useEffect is set to track state variable changes to: selectedYear and dataType
+     Conditional checks to make sure state variables are not empty strings, and then runs the API Call.
+     API only requires year when schedule data is requested. 
+          Conditional checks and runs different url if user selects Race Schedule.
+  */
   useEffect(() => {
     const apiFetch = async (userYearRequest, userTypeRequest) => {
-      const response = await fetch(
-        `https://ergast.com/api/f1/${userYearRequest}/${userTypeRequest}.json`
-      );
-      const data = await response.json();
-
-      console.log(data);
+      if (userTypeRequest === "schedule") {
+        const response = await fetch(
+          `https://ergast.com/api/f1/${userYearRequest}.json`
+        );
+        const data = await response.json();
+        setRaces(data.MRData.RaceTable.Races);
+      } else {
+        const response = await fetch(
+          `https://ergast.com/api/f1/${userYearRequest}/${userTypeRequest}.json`
+        );
+        const data = await response.json();
+        setDrivers(data.MRData.DriverTable.Drivers);
+      }
     };
     if (selectedYear !== "" && dataType !== "") {
       apiFetch(selectedYear, dataType);
@@ -46,20 +61,30 @@ function App() {
   const updateDataType = (dataQuery) => {
     setDataType(dataQuery);
   };
-  console.log("data Type:", dataType);
+
   // Param: yearSelected --> This parameter is the user's choice for what year he wants. Received from Home Component;
   // updateYear passed into Home Component as reference.
   const updateYear = (yearSelected) => {
     setSelectedYear(yearSelected);
   };
-  console.log("Selected Year:", selectedYear);
 
   return (
     <div>
-      <Home
-        handleSelectedYear={updateYear}
-        handleSelectedDataType={updateDataType}
-      />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home
+              handleSelectedYear={updateYear}
+              handleSelectedDataType={updateDataType}
+              driverInfo={drivers}
+              raceInfo={races}
+            />
+          }
+        ></Route>
+        <Route path="/About" element={<About />}></Route>
+        <Route path="/Contact" element={<Contact />}></Route>
+      </Routes>
     </div>
   );
 }
