f9317e5091f2ff5cf7dcc5af74e0aeb6583f823c
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-02-16 21:23:45 -0700

    Template for contact form

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,10 +1,24 @@
-import React from 'react';
-
+import React from "react";
 
 const Contact = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <form className="contact" onSubmit={handleSubmit}>
+        <div className="contact-name">
+          <label htmlFor="name">Name</label>
+          <input type="text" className="" />
         </div>
-    )
-}
+        <div className="contact-email">
+          <label htmlFor="email-input">Email Address</label>
+          <input type="email" className="" />
+        </div>
+        <div className="contact-message">
+          <label htmlFor="message">Message</label>
+          <textarea rows="5" type="text"></textarea>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
