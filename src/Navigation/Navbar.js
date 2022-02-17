028f4df14f96dd53408ed965cfad72bbdb813da7
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-02-16 19:46:53 -0700

    Added links logo and title.

--- src/Navigation/Navbar.js
+++ src/Navigation/Navbar.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React from "react";
+import {Link} from 'react-router-dom';
+import Home from "../Home/HomePage";
+import Contact from "../Contact/Contact";
+import About from "../About/About";
 
 
 const NavBar = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="nav-bar">
+      <div className="nav-bar-logo">
+          <img src="" alt="" />
+      </div>
+      <div className="nav-bar-title">
+        <span>Formula 1</span>
+      </div>
+      <div className="nav-bar-menu">
+        <Link to='/' element={<Home />}>Home</Link>
+        <Link to="/Contact" element={<Contact />}>
+          Contact
+        </Link>
+        <Link to="/About" element={<About />}>
+          About
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+
+export default NavBar;
