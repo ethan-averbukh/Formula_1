a7a9cb02104c05191277bf22209e245845acd3d0
eitan <79340133+eitan0991@users.noreply.github.com>
2022-02-18 03:41:01 -0800

    Added Dropdown menu for mobile view. Added responsiveness to navbar

---
+++ src/Navigation/Dropdown.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import {Link} from 'react-router-dom';
+import About from '../About/About';
+import Contact from '../Contact/Contact';
+import Home from '../Home/HomePage';
+import CloseIcon from '@mui/icons-material/Close';
+
+
+
+const Dropdown = ({handleClose}) => {
+   
+    const handleClick = () => {
+        handleClose();
+    }
+    return (
+        <div id="DropDown" className='drop-down'>
+            <CloseIcon className='close-icon' fontSize='medium' onClick={handleClick}></CloseIcon>
+            <Link onClick={handleClick} to='/' element={<Home />}>Home</Link>
+            <Link onClick={handleClick} to="/Contact" element={<Contact />}>
+                Contact
+            </Link>
+            <Link onClick={handleClick} to="/About" element={<About />}>
+                About
+            </Link>
+        </div>
+    )
+}
+
+export default Dropdown;
--- src/Navigation/Navbar.js
+++ src/Navigation/Navbar.js
@@ -1,27 +1,42 @@
-import React from "react";
-import {Link} from 'react-router-dom';
+import React, {useState} from "react";
+import { Link } from 'react-router-dom';
 import Home from "../Home/HomePage";
 import Contact from "../Contact/Contact";
 import About from "../About/About";
-
+import MenuIcon from '@mui/icons-material/Menu';
+import Dropdown from "./Dropdown";
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = (event) => {
+    setIsOpen(true);
+  }
+  const closeDropDown = () => {
+    setIsOpen(false);
+  }
   return (
     <div className="nav-bar">
       <div className="nav-bar-logo">
-          <img src="" alt="" />
+        <img src="" alt="" />
       </div>
       <div className="nav-bar-title">
         <span>Formula 1</span>
       </div>
       <div className="nav-bar-menu">
-        <Link to='/' element={<Home />}>Home</Link>
-        <Link to="/Contact" element={<Contact />}>
-          Contact
-        </Link>
-        <Link to="/About" element={<About />}>
-          About
-        </Link>
+        <MenuIcon 
+            fontSize="large" 
+            className="nav-bar-menu-icon" 
+            onClick={handleClick} />
+        {isOpen && <Dropdown handleClose={closeDropDown}/>}
+        <div className="nav-bar-menu-items">
+          <Link to='/' element={<Home />}>Home</Link>
+          <Link to="/Contact" element={<Contact />}>
+            Contact
+          </Link>
+          <Link to="/About" element={<About />}>
+            About
+          </Link>
+        </div>
       </div>
     </div>
   );
