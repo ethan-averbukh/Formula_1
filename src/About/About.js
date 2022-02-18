5b164c16bb688eed459fd6d4150c918123146ebb
eitan <79340133+eitan0991@users.noreply.github.com>
2022-02-18 07:43:04 -0800

    Added links to teams page when clicking on image.

--- src/About/About.js
+++ src/About/About.js
@@ -27,16 +27,16 @@ const About = () => {
       <div className="about-teams">
         <h2 className="about-teams-title">Teams</h2>
         <div className="about-teams-grid">
-          <div className="team"><a href=""><img src={alfa} alt="" /></a></div>
-          <div className="team"><a href=""><img src={alpha} alt="" /></a></div>
-          <div className="team"><a href=""><img src={alpine} alt="" /></a></div>
-          <div className="team"><a href=""><img src={aston} alt="" /></a></div>
-          <div className="team"><a href=""><img src={ferrari} alt="" /></a></div>
-          <div className="team"><a href=""><img src={haas} alt="" /></a></div>
-          <div className="team"><a href=""><img src={mclaren} alt="" /></a></div>
-          <div className="team"><a href=""><img src={mercedes} alt="" /></a></div>
-          <div className="team"><a href=""><img src={redbull} alt="" /></a></div>
-          <div className="team"><a href=""><img src={williams} alt="" /></a></div>
+          <div className="team"><a href="https://www.sauber-group.com/motorsport/formula-1/"><img src={alfa} alt="" /></a></div>
+          <div className="team"><a href="https://scuderia.alphatauri.com/en/"><img src={alpha} alt="" /></a></div>
+          <div className="team"><a href="https://www.alpinecars.com/en/formula-1/news/"><img src={alpine} alt="" /></a></div>
+          <div className="team"><a href="https://www.astonmartinf1.com/en-GB/"><img src={aston} alt="" /></a></div>
+          <div className="team"><a href="https://www.ferrari.com/en-EN/formula1"><img src={ferrari} alt="" /></a></div>
+          <div className="team"><a href="https://www.haasf1team.com/"><img src={haas} alt="" /></a></div>
+          <div className="team"><a href="https://www.mclaren.com/racing/"><img src={mclaren} alt="" /></a></div>
+          <div className="team"><a href="https://www.mercedesamgf1.com/en/"><img src={mercedes} alt="" /></a></div>
+          <div className="team"><a href="https://www.redbullracing.com/int-en"><img src={redbull} alt="" /></a></div>
+          <div className="team"><a href="https://www.williamsf1.com/"><img src={williams} alt="" /></a></div>
         </div>
       </div>
     </div>
